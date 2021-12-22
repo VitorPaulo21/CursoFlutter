e6051d691f035101414d824fe5d0609e6ce17ff3
Vitor Medeiros <vitorpaulo3121@gmail.com>
2021-12-22 04:25:15 -0300

    Update Shop Aula 1

---
+++ shop/lib/screens/products_overview_screen.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:shop/data/dummy_data.dart';
+import 'package:shop/models/product.dart';
+
+class ProductOverviewPage extends StatelessWidget {
+  const ProductOverviewPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    List<Product> loadedProducts = DUMMY_PRODUCTS;
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Minha Loja"),
+        centerTitle: true,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(10.0),
+        child: GridView.builder(
+          itemCount: loadedProducts.length,
+          itemBuilder: (ctx, i) {
+            return Text(loadedProducts[i].title);
+          },
+          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 2,
+            childAspectRatio: 3 / 2,
+            crossAxisSpacing: 10,
+            mainAxisSpacing: 10,
+          ),
+        ),
+      ),
+    );
+  }
+}
